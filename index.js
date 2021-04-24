f81e3ea880ced0fe1fbfa699d95d6e1d26aad0af
tishalewis8707 <tishalewis8707@gmail.com>
2021-04-24 19:01:03 -0400

    Stlye border

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const getJokes = () => {
-  return fetch('https://official-joke-api.appspot.com/random_joke')
-      .then(res => res.json())
-}
+const button = document.querySelector('.container button');
+const jokeText = document.querySelector('.container p');
 
+button.addEventListener('click', getJoke);
 
-
-
-const createJokeDiv = (joke) => { 
-  const jokeContainer = document.createElement('div')
-  const jokeContainerTitle = document.createElement('h2')
-  const setup = document.createElement('p')
-  const punchline = document.createElement('p')
-  const jokeRating = document.createElement('p')
-  const ratingInput = document.createElement('input')
-  const ratingBtn = document.createElement('button')
