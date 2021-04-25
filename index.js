4d635b55c8f94146ff07ac18f83c31d67e8d385a
tishalewis8707 <tishalewis8707@gmail.com>
2021-04-24 23:11:52 -0400

    yes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const button = document.querySelector('.container button');
 const jokeText = document.querySelector('.container p');
 
-button.addEventListener('click', getJoke);
+document.addEventListener('DOMContentLoaded', getJoke);
+
+
+function getJoke(){
+  fetch(' https://icanhazdadjoke.com/',{
+    headers:{
+      'Accept': 'application/json'
+    }
+  }).then(Response=> Response.json())
+  .then(getJoke=> console.log(getJoke)
+
+  )};
+
+
+
+//async function getJoke(){
+  //const jokeData = await fetch('https://official-joke-api.appspot.com/jokes/random', {
+  //Headers: {
+    //'Accept': 'application/json'
+  //}
+  //});
+  //const jokeObj = await jokeData.json();
+//jokeText.innerHTML = jokeObj.joke;
+//}
 
